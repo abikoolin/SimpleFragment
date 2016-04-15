87da8e08ada0312b6fca45a2162a1eb615dfe3e7
Abishek Mahadevaswamy <Abishek.M@happiestminds.com>
2016-04-15 15:32:27 +0530

    changed for tags

--- HelloSbt.scala
+++ HelloSbt.scala
@@ -1,3 +1,3 @@
 class Main {
-	def sayHi() = println("Hello SBT! -- added to test edit in git --this added again to update from local to github")
+	def sayHi() = println("Hello SBT! -- added to test edit in git --this added again to update from local to github --for tags")
 }
